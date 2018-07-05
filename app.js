c9e4c0a2f374916d38d63c389bfa2870a6e6dc70
Andrew Bales <agbales@gmail.com>
2018-07-05 12:58:09 -0700

    API spending route

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+const spendingRoutes = require('./api/routes/spending');
+
+app.use('/spending', spendingRoutes);
+
 app.use((req, res, next) => {
     res.status(200).json({
         message: "Got it"

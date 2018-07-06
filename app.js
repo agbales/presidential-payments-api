08074b6dd8244fa05e3224e9388dc7240de6d355
Andrew Bales <agbales@gmail.com>
2018-07-06 03:33:28 -0700

    start handling various q2m params

--- app.js
+++ app.js
@@ -10,12 +10,17 @@ app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
     let queries = q2m(req.query);
-    let criteria = queries.critera;
-    console.log(queries);
+    let criteria = queries.criteria;
+    let numberOfParmas = Object.keys(criteria).length;
 
-    //{  amount: { $gt: 100 } }
-    // { $and: [{ price: { $ne: 1.99 } }, { price: { $exists: true } }] }
+    if (numberOfParmas > 1) {
+        // CONCAT PARAM { $and: [{ price: { $ne: 1.99 } }, { price: { $exists: true } }] }
+    } else {
+        // SINGLE PARAM {  amount: { $gt: 100 } }
+    }
 
+    console.log("criteria", criteria);
+    
     const uri = "mongodb+srv://agbales:" + process.env.MONGO_ATLAS_PW + "@trump-spending-bbdqf.gcp.mongodb.net/propublica_trump_spending";
     mongo.connect(uri, { useNewUrlParser: false }, function(err, client) {
         const collection = client.db("trump-spending").collection("propublica_trump_spending");

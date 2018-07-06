9854ba0a4263705d4a96604e4b2ae60e5386b3a2
Andrew Bales <agbales@gmail.com>
2018-07-06 02:49:40 -0700

    filtering params

--- app.js
+++ app.js
@@ -9,17 +9,26 @@ app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
     let queries = req.query;
-    console.log(queries);
-
+    let params = Object.entries(queries).map(query => {
+        let key = query[0];
+        let value = query[1]
+        let obj = {}
+        if (key == "amount") {
+            obj.amount = {};
+            object.amount[Symbol($gt)] = parseInt(value);
+        } else {
+            obj[key] = value;
+        }
+        return obj;
+    });
+    console.log(params);
+    //{ amount: { $gt: 100 } }
     // db.inventory.find( { $and: [ { price: { $ne: 1.99 } }, { price: { $exists: true } } ] } )
 
     const uri = "mongodb+srv://agbales:" + process.env.MONGO_ATLAS_PW + "@trump-spending-bbdqf.gcp.mongodb.net/propublica_trump_spending";
     mongo.connect(uri, { useNewUrlParser: false }, function(err, client) {
         const collection = client.db("trump-spending").collection("propublica_trump_spending");
-        collection.find({ amount: { $gt: 100 } })
-            // .forEach( function(results) { 
-
-            // });
+        collection.find(params).limit(5)
             .toArray()
             .then(resp => {
                 res.status(200).json({item: resp})

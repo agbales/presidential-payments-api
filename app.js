4800c545c9166a5c3b3bbe53fe33d9fb1feecfbf
Andrew Bales <agbales@gmail.com>
2018-07-06 01:07:47 -0700

    root responding with collection data

--- app.js
+++ app.js
@@ -1,31 +1,28 @@
 const express = require('express');
 const router = express.Router();
 const app = express();
+const mongo = require('mongodb').MongoClient;
 
 // logging
 const morgan = require('morgan'); 
 app.use(morgan('dev'));
 
-const mongo = require('mongodb').MongoClient;
-const uri = "mongodb+srv://agbales:" + process.env.MONGO_ATLAS_PW + "@trump-spending-bbdqf.gcp.mongodb.net/propublica_trump_spending";
-mongo.connect(uri, { useNewUrlParser: false }, function(err, client) {
-    const collection = client.db("trump-spending").collection("propublica_trump_spending");
-
-    collection.findOne({type:"FEC"})
-        .then(resp => {
-            console.log(resp)
-            // res.status(200).json({message: resp})
-        })
-        .catch(err => {
-            res.status(500).json({
-                message: err
-            });
-        });
-    client.close();
-});
-
 app.get('/', (req, res) => {
-
+    const uri = "mongodb+srv://agbales:" + process.env.MONGO_ATLAS_PW + "@trump-spending-bbdqf.gcp.mongodb.net/propublica_trump_spending";
+    mongo.connect(uri, { useNewUrlParser: false }, function(err, client) {
+        const collection = client.db("trump-spending").collection("propublica_trump_spending");
+        collection.findOne({type:"FEC"})
+            .then(resp => {
+                console.log(resp)
+                res.status(200).json({message: resp})
+            })
+            .catch(err => {
+                res.status(500).json({
+                    message: err
+                });
+            });
+        client.close();
+    });
 });
 
 module.exports = app;

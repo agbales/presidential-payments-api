78e9f5cb47cb6f5b853164f14394553d36a9e700
Andrew Bales <agbales@gmail.com>
2018-07-06 13:01:52 -0700

    multiple query handling

--- app.js
+++ app.js
@@ -9,26 +9,30 @@ const morgan = require('morgan');
 app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
-    let queries = q2m(req.query);
-    let criteria = queries.criteria;
-    let numberOfParmas = Object.keys(criteria).length;
+    let queries = req.query;
+    let mongoQueries = q2m(queries);
+    let criteria = mongoQueries.criteria;
 
-    if (numberOfParmas > 1) {
-        // CONCAT PARAM { $and: [{ price: { $ne: 1.99 } }, { price: { $exists: true } }] }
-    } else {
-        // SINGLE PARAM {  amount: { $gt: 100 } }
-    }
+    // let numberOfParmas = Object.keys(criteria).length;
+    // if (numberOfParmas > 1) {
+    //     // CONCAT PARAM { $and: [{ price: { $ne: 1.99 } }, { price: { $exists: true } }] }
+    // }
+    // Otherwise...
+    // SINGLE PARAM {  amount: { $gt: 100 } }
 
-    console.log("criteria", criteria);
+    console.log("Search Citeria : ", criteria);
 
     const uri = "mongodb+srv://agbales:" + process.env.MONGO_ATLAS_PW + "@trump-spending-bbdqf.gcp.mongodb.net/propublica_trump_spending";
     mongo.connect(uri, { useNewUrlParser: false }, function(err, client) {
         const collection = client.db("trump-spending").collection("propublica_trump_spending");
-        // limiting 10 for now
-        collection.find(criteria).limit(10)
+        collection.find(criteria)
             .toArray()
             .then(resp => {
-                res.status(200).json({item: resp})
+                res.status(200).json({ 
+                    query: criteria,
+                    response_total: resp.length,
+                    expenditures: resp
+                })
                 client.close();
             })
             .catch(err => {

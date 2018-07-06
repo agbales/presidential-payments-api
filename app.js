63ac0fe4e7f997fea756badd5d6e32ae06e933f4
Andrew Bales <agbales@gmail.com>
2018-07-06 03:21:08 -0700

    q2m queries replacing hand coded

--- app.js
+++ app.js
@@ -2,33 +2,24 @@ const express = require('express');
 const router = express.Router();
 const app = express();
 const mongo = require('mongodb').MongoClient;
+var q2m = require('query-to-mongo')
 
 // logging
 const morgan = require('morgan'); 
 app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
-    let queries = req.query;
-    let params = Object.entries(queries).map(query => {
-        let key = query[0];
-        let value = query[1]
-        let obj = {}
-        if (key == "amount") {
-            obj.amount = {};
-            object.amount[Symbol($gt)] = parseInt(value);
-        } else {
-            obj[key] = value;
-        }
-        return obj;
-    });
-    console.log(params);
-    //{ amount: { $gt: 100 } }
-    // db.inventory.find( { $and: [ { price: { $ne: 1.99 } }, { price: { $exists: true } } ] } )
+    let queries = q2m(req.query);
+    let criteria = queries.critera;
+    console.log(queries);
+
+    //{  amount: { $gt: 100 } }
+    // { $and: [{ price: { $ne: 1.99 } }, { price: { $exists: true } }] }
 
     const uri = "mongodb+srv://agbales:" + process.env.MONGO_ATLAS_PW + "@trump-spending-bbdqf.gcp.mongodb.net/propublica_trump_spending";
     mongo.connect(uri, { useNewUrlParser: false }, function(err, client) {
         const collection = client.db("trump-spending").collection("propublica_trump_spending");
-        collection.find(params).limit(5)
+        collection.find(criteria).limit(5)
             .toArray()
             .then(resp => {
                 res.status(200).json({item: resp})

b21091800853e4461c7a6e1761e02e166dd45520
Andrew Bales <agbales@gmail.com>
2018-07-08 10:15:03 -0700

    removed 'distinct' comments

--- app.js
+++ app.js
@@ -19,23 +19,4 @@ app.use((req, res, next) => {
 app.use('/distinct', distinctRoute);
 app.use('/expenditures', expendituresRoute);
 
-
-// - - - - - - - Look for distinct value requests EX: "type=distinct"
-
-// Object.entries(criteria).forEach(entry => {
-//     let key = entry[0];
-//     let value = entry[1];
-//     if (value == "distinct") {
-//         collection.distinct(key)
-//             .then(resp => {
-//                 let distinct = { [key] : resp }
-//                 console.log(distinct);
-//                 // Finding a way to not disrupt rest of queries...
-//                 // May need to be its own route
-//                 delete criteria[key];
-//             })
-//     }
-// })
-//------------------
-
 module.exports = app;

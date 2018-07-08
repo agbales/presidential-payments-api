0cdc75c74f5682afb645edf4799abec29dc4729c
Andrew Bales <agbales@gmail.com>
2018-07-08 07:12:07 -0700

    header updates

--- app.js
+++ app.js
@@ -2,12 +2,19 @@ const express = require('express');
 const router = express.Router();
 const app = express();
 const mongo = require('mongodb').MongoClient;
+
 var q2m = require('query-to-mongo')
 
 // logging
 const morgan = require('morgan'); 
 app.use(morgan('dev'));
 
+app.use((req, res) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', '*')
+});
+
+// Routes
 app.get('/', (req, res) => {
     let queries = req.query;
     let mongoQueries = q2m(queries);
@@ -19,26 +26,26 @@ app.get('/', (req, res) => {
     const uri = "mongodb+srv://agbales:" + process.env.MONGO_ATLAS_PW + "@trump-spending-bbdqf.gcp.mongodb.net/propublica_trump_spending";
     mongo.connect(uri, { useNewUrlParser: false }, function(err, client) {
         const collection = client.db("trump-spending").collection("propublica_trump_spending");
-
-        // - - - - - - - Look for distinct value requests EX: "type=all"
-
-        Object.entries(criteria).forEach(entry => {
-            let key = entry[0];
-            let value = entry[1];
-            if (value == "all") {
-                collection.distinct(key)
-                    .then(resp => {
-                        let distinct = { [key] : resp }
-                        console.log(distinct);
-                        // Finding a way to not disrupt rest of queries...
-                        // May need to be its own route
-                        // delete criteria[key];
-                    })
-            }
-        })
         
-        // - - - - - - -
+         // - - - - - - - Look for distinct value requests EX: "type=distinct"
 
+        // Object.entries(criteria).forEach(entry => {
+        //     let key = entry[0];
+        //     let value = entry[1];
+        //     if (value == "distinct") {
+        //         collection.distinct(key)
+        //             .then(resp => {
+        //                 let distinct = { [key] : resp }
+        //                 console.log(distinct);
+        //                 // Finding a way to not disrupt rest of queries...
+        //                 // May need to be its own route
+        //                 delete criteria[key];
+        //             })
+        //     }
+        // })
+
+        // - - - - - - -
+        
         collection.find(criteria)
             .toArray()
             .then(resp => {

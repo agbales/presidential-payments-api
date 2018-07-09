c06525db1a5b09b26768b0ea9b8f5d8d9dcaadcb
Andrew Bales <agbales@gmail.com>
2018-07-08 17:31:41 -0700

    adding swagger docs

--- app.js
+++ app.js
@@ -1,22 +1,23 @@
 const express = require('express');
 const router = express.Router();
 const app = express();
+const morgan = require('morgan'); 
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./api/swagger/swagger.yaml');
 const distinctRoute = require('./api/routes/distinct');
 const expendituresRoute = require('./api/routes/expenditures');
 
-// Logging
-const morgan = require('morgan'); 
 app.use(morgan('dev'));
 
-// Headers
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', '*');
     next();
 });
 
-// Routes
-app.use('/static', express.static(path.join(__dirname, 'public')))
+app.use('/', express.static('public'))
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/distinct', distinctRoute);
 app.use('/expenditures', expendituresRoute);
 

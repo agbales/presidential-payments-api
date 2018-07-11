322803ec39c9618f0e7a0a1807673ba1e9643d04
Andrew Bales <agbales@gmail.com>
2018-07-10 19:23:19 -0700

    distinct schema verfication

--- test/tests.js
+++ test/tests.js
@@ -6,59 +6,45 @@ const chai = require('chai')
 chai.use(require('chai-json-schema'));
 chai.use(chaiHttp);
 
-const expendituresSchema =  {
-  _id: "string",
-  type: "string",
-  source: "string",
-  date: "string",
-  amount: "number",
-  purpose_scrubbed: "string",
-  property_scrubbed: "string",
-  purpose: "string",
-  property: "string",
-  city: "string",
-  state: "string"
-};
-
-
 describe('Expenditures', function() {
     it('should list ALL expenditures on /expenditures GET', function(done) {
         chai.request(app)
-            .get('/expenditures')
-            .end(function(err, res){
-              res.should.have.status(200);
-              res.should.be.json;
-              res.body.should.be.a('object');
-              res.body.should.have.property('response_total');
-              res.body.response_total.should.be.a('number'); 
-              res.body.should.have.property('expenditures');
-              res.body.expenditures.should.be.a('array');
-
-              
-              res.body.expenditures.every(expenditure => { 
-                expenditure.should.be.a('object');
-                expenditure.should.have.all.keys(['_id', 'type', 'source', 'date', 'amount', 
-                                                  'purpose_scrubbed', 'property_scrubbed', 'purpose', 
-                                                  'property', 'city', 'state']);
-                // Above 'keys' works, but schema has error in syntax...
-                // expenditure.should.be.jsonSchema(expendituresSchema);
-              });
-
-              done();
+          .get('/expenditures')
+          .end(function(err, res){
+            res.should.have.status(200);
+            res.should.be.json;
+            res.body.should.be.a('object');
+            res.body.should.have.property('response_total');
+            res.body.response_total.should.be.a('number'); 
+            res.body.should.have.property('expenditures');
+            res.body.expenditures.should.be.a('array');
+            res.body.expenditures.every(expenditure => { 
+              expenditure.should.be.a('object');
+              expenditure.should.have.all.keys(['_id', 'type', 'source', 'date', 'amount', 
+                                                'purpose_scrubbed', 'property_scrubbed', 'purpose', 
+                                                'property', 'city', 'state']);
             });
+            done();
+          });
     });
 });
 
-
 describe('Distinct', function() {
   it('should list ALL distinct on /distinct GET', function(done) {
+    this.timeout(15000);
     chai.request(app)
-    .get('/expenditures')
-    .end(function(err, res){
-      res.should.have.status(200);
-      res.should.be.json;
-      res.body.should.be.a('object');
-    });
-    done();
+      .get('/distinct')
+      .end(function(err, res){
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('array');
+        res.body.every(entry => { 
+          let key = Object.keys(entry);
+          entry.should.be.a('object');
+          [key].should.have.lengthOf(1);
+          [key][0].should.not.be.empty;
+        });
+        done();
+      });
   });
 });

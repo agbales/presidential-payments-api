de5dfaa8f3df09638dccba127c5bf659978b8637
Andrew Bales <agbales@gmail.com>
2018-07-10 22:16:48 -0700

    added error handling tests

--- test/tests.js
+++ test/tests.js
@@ -7,8 +7,9 @@ chai.use(require('chai-json-schema'));
 chai.use(chaiHttp);
 
 describe('Expenditures', function() {
+    this.timeout(5000);
+
     it('should returns JSON object on /expenditures GET', function(done) {
-      this.timeout(5000);
       chai.request(app)
         .get('/expenditures')
         .end(function(err, res){
@@ -20,7 +21,6 @@ describe('Expenditures', function() {
     });
 
     it('should return numeric response_total /expenditures GET', function(done) {
-      this.timeout(5000)
       chai.request(app)
         .get('/expenditures')
         .end(function(err, res){
@@ -31,7 +31,6 @@ describe('Expenditures', function() {
     });
 
     it('should return array of expenditures in proper schema /expenditures GET', function(done) {
-      this.timeout(5000)
       chai.request(app)
         .get('/expenditures')
         .end(function(err, res){
@@ -46,11 +45,56 @@ describe('Expenditures', function() {
           done();
         })
     });
+
+    it('should return 200 response for valid queries on /expenditures GET', function(done) {
+      chai.request(app)
+        .get('/expenditures?amount>1000')
+        .end(function(err, res){
+          res.should.have.status(200);
+          res.should.be.json;
+          done();
+        });
+    });
+
+    it('should return 200 response for mulitple queries on /expenditures GET', function(done) {
+      chai.request(app)
+        .get('/expenditures?amount>1000&type=FEC')
+        .end(function(err, res){
+          res.should.have.status(200);
+          res.should.be.json;
+          done();
+        });
+    });
+
+    it('should return 400 Error for an invalid query on /expenditures GET', function(done) {
+      chai.request(app)
+        .get('/expenditures?kittens=100')
+        .end(function(err, res){
+          res.should.have.status(400);
+          res.should.be.json;
+          res.body.should.be.a('object');
+          done();
+        });
+    });
+
+    it('should return 400 Error for invalid query alongside valid queries on /expenditures GET', function(done) {
+      chai.request(app)
+        .get('/expenditures?amount=9000&kittens=100')
+        .end(function(err, res){
+          res.should.have.status(400);
+          res.should.be.json;
+          res.body.should.be.a('object');
+          done();
+        });
+    });
+
+
 });
 
 describe('Distinct', function() {
+  this.timeout(5000);
+
   it('should return a JSON array on /distinct GET', function(done) {
-    this.timeout(5000);
     chai.request(app)
       .get('/distinct')
       .end(function(err, res){
@@ -62,7 +106,6 @@ describe('Distinct', function() {
   });
 
   it('should contain objects with one key/value pair where value is non-empty array on /distinct GET', function(done) {
-    this.timeout(5000);
     chai.request(app)
       .get('/distinct')
       .end(function(err, res){

e8477fc03ba8c41e673d4537165ec49e6194de2a
Andrew Bales <agbales@gmail.com>
2018-07-10 21:56:37 -0700

    query validator

--- api/routes/expenditures.js
+++ api/routes/expenditures.js
@@ -5,11 +5,26 @@ const q2m = require('query-to-mongo');
 require('dotenv').config()
 const uri = 'mongodb+srv://agbales:' + process.env.MONGO_ATLAS_PW + '@trump-spending-bbdqf.gcp.mongodb.net/propublica_trump_spending';
 
-router.get('/', function(req, res) {
+function isQueryValid(queries) {
+    let validity = true;
+    let keys = Object.keys(queries);
+    let validKeys = ['type', 'source', 'date', 'amount', 'purpose_scrubbed', 'property_scrubbed', 'purpose', 'property', 'city', 'state'];
+    keys.forEach(key=>{
+        if (!validKeys.includes(key.toLowerCase())) {
+            validity = false;
+        }
+    });
+    return validity;
+}
 
+router.get('/', function(req, res) {
     let queries = req.query;
     let mongoQueries = q2m(queries);
     let criteria = mongoQueries.criteria;
+
+    if(!isQueryValid(criteria)) {
+        return res.status(400).send({code: 'BadRequest', message: 'Invalid query present. See /distinct.'})
+    }
 
     mongo.connect(uri, { useNewUrlParser: false }, function(err, client) {
         const collection = client.db('trump-spending').collection('propublica_trump_spending');

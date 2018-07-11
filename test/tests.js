75dc0ee1b58fa28faca4ad4f561b06fdf9f84dff
Andrew Bales <agbales@gmail.com>
2018-07-10 19:28:21 -0700

    refactoring tests

--- test/tests.js
+++ test/tests.js
@@ -8,30 +8,31 @@ chai.use(chaiHttp);
 
 describe('Expenditures', function() {
     it('should list ALL expenditures on /expenditures GET', function(done) {
-        chai.request(app)
-          .get('/expenditures')
-          .end(function(err, res){
-            res.should.have.status(200);
-            res.should.be.json;
-            res.body.should.be.a('object');
-            res.body.should.have.property('response_total');
-            res.body.response_total.should.be.a('number'); 
-            res.body.should.have.property('expenditures');
-            res.body.expenditures.should.be.a('array');
-            res.body.expenditures.every(expenditure => { 
-              expenditure.should.be.a('object');
-              expenditure.should.have.all.keys(['_id', 'type', 'source', 'date', 'amount', 
-                                                'purpose_scrubbed', 'property_scrubbed', 'purpose', 
-                                                'property', 'city', 'state']);
-            });
-            done();
+      this.timeout(5000);
+      chai.request(app)
+        .get('/expenditures')
+        .end(function(err, res){
+          res.should.have.status(200);
+          res.should.be.json;
+          res.body.should.be.a('object');
+          res.body.should.have.property('response_total');
+          res.body.response_total.should.be.a('number'); 
+          res.body.should.have.property('expenditures');
+          res.body.expenditures.should.be.a('array');
+          res.body.expenditures.every(expenditure => { 
+            expenditure.should.be.a('object');
+            expenditure.should.have.all.keys(['_id', 'type', 'source', 'date', 'amount', 
+                                              'purpose_scrubbed', 'property_scrubbed', 'purpose', 
+                                              'property', 'city', 'state']);
           });
+          done();
+        });
     });
 });
 
 describe('Distinct', function() {
   it('should list ALL distinct on /distinct GET', function(done) {
-    this.timeout(15000);
+    this.timeout(5000);
     chai.request(app)
       .get('/distinct')
       .end(function(err, res){
